7da6f6a088b8fd18c068003a2ba1dbd274da0a2a
afelafifah <afelafifah@gmail.com>
2018-12-01 22:05:11 +0700

    versi 1.0.0

---
+++ application/logs/log-2018-11-30.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-11-30 04:17:07 --> 404 Page Not Found: /index
---
+++ application/logs/log-2018-12-01.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-12-01 15:51:02 --> 404 Page Not Found: /index
